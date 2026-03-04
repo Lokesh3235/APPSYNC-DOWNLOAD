c1531f7f0cfa661796535cfa78134822830b5b25
Lokesh Kumar <7093lokesh@gmail.com>
2026-03-04 11:27:01 +0000

    chore: update AppSync schema + types + operations (Wed Mar  4 11:27:01 UTC 2026)

--- mutations.js
+++ mutations.js
@@ -3,31 +3,31 @@
 
 
 export const EditOrganization = /* GraphQL */ `
-  mutation EditOrganization($input: [object Object]) {
+  mutation EditOrganization($input: EditOrganizationInput) {
     EditOrganization(input: $input)
   }
 `;
 
 export const GenerateExcelReport = /* GraphQL */ `
-  mutation GenerateExcelReport($input: [object Object]) {
+  mutation GenerateExcelReport($input: GenerateExcelReportInput) {
     GenerateExcelReport(input: $input)
   }
 `;
 
 export const UserApprovals = /* GraphQL */ `
-  mutation UserApprovals($input: [object Object]) {
+  mutation UserApprovals($input: UserApprovalsInput) {
     UserApprovals(input: $input)
   }
 `;
 
 export const addCreditsToUser = /* GraphQL */ `
-  mutation AddCreditsToUser($input: [object Object]) {
+  mutation AddCreditsToUser($input: addCreditsToUserInput) {
     addCreditsToUser(input: $input)
   }
 `;
 
 export const addOrRemoveQuestionToQuiz = /* GraphQL */ `
-  mutation AddOrRemoveQuestionToQuiz($input: [object Object]) {
+  mutation AddOrRemoveQuestionToQuiz($input: addQuestionToQuizInput) {
     addOrRemoveQuestionToQuiz(input: $input)
   }
 `;
@@ -39,139 +39,139 @@ export const clearErrorLogs = /* GraphQL */ `
 `;
 
 export const createCategory = /* GraphQL */ `
-  mutation CreateCategory($input: [object Object]) {
+  mutation CreateCategory($input: createCategoryInput) {
     createCategory(input: $input)
   }
 `;
 
 export const createQuestion = /* GraphQL */ `
-  mutation CreateQuestion($input: [object Object]) {
+  mutation CreateQuestion($input: createQuestionInput) {
     createQuestion(input: $input)
   }
 `;
 
 export const createQuiz = /* GraphQL */ `
-  mutation CreateQuiz($input: [object Object]) {
+  mutation CreateQuiz($input: createQuizInput) {
     createQuiz(input: $input)
   }
 `;
 
 export const createQuizarreQuizRun = /* GraphQL */ `
-  mutation CreateQuizarreQuizRun($input: [object Object]) {
+  mutation CreateQuizarreQuizRun($input: createQuizarreQuizRunInput) {
     createQuizarreQuizRun(input: $input)
   }
 `;
 
 export const createTeam = /* GraphQL */ `
-  mutation CreateTeam($input: [object Object]) {
+  mutation CreateTeam($input: createTeamInput) {
     createTeam(input: $input)
   }
 `;
 
 export const createUser = /* GraphQL */ `
-  mutation CreateUser($input: [object Object]) {
+  mutation CreateUser($input: createUserInput) {
     createUser(input: $input)
   }
 `;
 
 export const deductCreditsFromUser = /* GraphQL */ `
-  mutation DeductCreditsFromUser($input: [object Object]) {
+  mutation DeductCreditsFromUser($input: deductCreditsFromUserInput) {
     deductCreditsFromUser(input: $input)
   }
 `;
 
 export const editCategory = /* GraphQL */ `
-  mutation EditCategory($input: [object Object]) {
+  mutation EditCategory($input: editCategoryInput) {
     editCategory(input: $input)
   }
 `;
 
 export const editPendingReviewQuestions = /* GraphQL */ `
-  mutation EditPendingReviewQuestions($input: [object Object]) {
+  mutation EditPendingReviewQuestions($input: editQuestionInput) {
     editPendingReviewQuestions(input: $input)
   }
 `;
 
 export const editQuestion = /* GraphQL */ `
-  mutation EditQuestion($input: [object Object]) {
+  mutation EditQuestion($input: editQuestionInput) {
     editQuestion(input: $input)
   }
 `;
 
 export const editQuiz = /* GraphQL */ `
-  mutation EditQuiz($input: [object Object]) {
+  mutation EditQuiz($input: editQuizInput) {
     editQuiz(input: $input)
   }
 `;
 
 export const editQuizRunLeaderboard = /* GraphQL */ `
-  mutation EditQuizRunLeaderboard($input: [object Object]) {
+  mutation EditQuizRunLeaderboard($input: editQuizRunLeaderboardInput) {
     editQuizRunLeaderboard(input: $input)
   }
 `;
 
 export const editQuizarreQuizRun = /* GraphQL */ `
-  mutation EditQuizarreQuizRun($input: [object Object]) {
+  mutation EditQuizarreQuizRun($input: editQuizarreQuizRunInput) {
     editQuizarreQuizRun(input: $input)
   }
 `;
 
 export const editTeam = /* GraphQL */ `
-  mutation EditTeam($input: [object Object]) {
+  mutation EditTeam($input: editTeamInput) {
     editTeam(input: $input)
   }
 `;
 
 export const editTeamUsers = /* GraphQL */ `
-  mutation EditTeamUsers($input: [object Object]) {
+  mutation EditTeamUsers($input: editTeamUsersInput) {
     editTeamUsers(input: $input)
   }
 `;
 
 export const editUser = /* GraphQL */ `
-  mutation EditUser($input: [object Object]) {
+  mutation EditUser($input: editUserInput) {
     editUser(input: $input)
   }
 `;
 
 export const generateBulkUploadUrl = /* GraphQL */ `
-  mutation GenerateBulkUploadUrl($input: [object Object]) {
+  mutation GenerateBulkUploadUrl($input: commons3Input) {
     generateBulkUploadUrl(input: $input)
   }
 `;
 
 export const generateS3UploadURL = /* GraphQL */ `
-  mutation GenerateS3UploadURL($input: [object Object]) {
+  mutation GenerateS3UploadURL($input: commons3Input) {
     generateS3UploadURL(input: $input)
   }
 `;
 
 export const publishQuizarreQuizRun = /* GraphQL */ `
-  mutation PublishQuizarreQuizRun($input: [object Object]) {
+  mutation PublishQuizarreQuizRun($input: publishQuizarreQuizRunInput) {
     publishQuizarreQuizRun(input: $input)
   }
 `;
 
 export const reorderQuizQuestions = /* GraphQL */ `
-  mutation ReorderQuizQuestions($input: [object Object]) {
+  mutation ReorderQuizQuestions($input: reorderQuizQuestionsInput) {
     reorderQuizQuestions(input: $input)
   }
 `;
 
 export const resetDashboardAndReports = /* GraphQL */ `
-  mutation ResetDashboardAndReports($input: [object Object]) {
+  mutation ResetDashboardAndReports($input: resetDashboardAndReportsInput) {
     resetDashboardAndReports(input: $input)
   }
 `;
 
 export const terminateLiveQuizRun = /* GraphQL */ `
-  mutation TerminateLiveQuizRun($quiz_run_id: [object Object]) {
+  mutation TerminateLiveQuizRun($quiz_run_id: String!) {
     terminateLiveQuizRun(quiz_run_id: $quiz_run_id)
   }
 `;
 
 export const updateBmsSettings = /* GraphQL */ `
-  mutation UpdateBmsSettings($input: [object Object]) {
+  mutation UpdateBmsSettings($input: updateBmsSettingsInput) {
     updateBmsSettings(input: $input)
   }
 `;
--- queries.js
+++ queries.js
@@ -3,25 +3,25 @@
 
 
 export const getCurrentUserDetails = /* GraphQL */ `
-  query GetCurrentUserDetails($input: [object Object]) {
+  query GetCurrentUserDetails($input: getCurrentUserDetailsInput) {
     getCurrentUserDetails(input: $input)
   }
 `;
 
 export const getDocumentPresignedURL = /* GraphQL */ `
-  query GetDocumentPresignedURL($input: [object Object]) {
+  query GetDocumentPresignedURL($input: commons3Input) {
     getDocumentPresignedURL(input: $input)
   }
 `;
 
 export const getOrganizationDetails = /* GraphQL */ `
-  query GetOrganizationDetails($input: [object Object]) {
+  query GetOrganizationDetails($input: getOrganizationDetailsInput) {
     getOrganizationDetails(input: $input)
   }
 `;
 
 export const listApprovals = /* GraphQL */ `
-  query ListApprovals($input: [object Object]) {
+  query ListApprovals($input: listApprovalsInput) {
     listApprovals(input: $input)
   }
 `;
@@ -33,157 +33,157 @@ export const listBmsSettings = /* GraphQL */ `
 `;
 
 export const listBulkUploadErrors = /* GraphQL */ `
-  query ListBulkUploadErrors($input: [object Object]) {
+  query ListBulkUploadErrors($input: listBulkUploadErrorsInput) {
     listBulkUploadErrors(input: $input)
   }
 `;
 
 export const listCategories = /* GraphQL */ `
-  query ListCategories($input: [object Object]) {
+  query ListCategories($input: listCategoriesInput) {
     listCategories(input: $input)
   }
 `;
 
 export const listCategoryReports = /* GraphQL */ `
-  query ListCategoryReports($input: [object Object]) {
+  query ListCategoryReports($input: listCategoryReportsInput) {
     listCategoryReports(input: $input)
   }
 `;
 
 export const listCreditsHistory = /* GraphQL */ `
-  query ListCreditsHistory($input: [object Object]) {
+  query ListCreditsHistory($input: listCreditsHistoryInput) {
     listCreditsHistory(input: $input)
   }
 `;
 
 export const listDeviceBrowserReports = /* GraphQL */ `
-  query ListDeviceBrowserReports($input: [object Object]) {
+  query ListDeviceBrowserReports($input: listDeviceBrowserReportsInput) {
     listDeviceBrowserReports(input: $input)
   }
 `;
 
 export const listLocationReports = /* GraphQL */ `
-  query ListLocationReports($input: [object Object]) {
+  query ListLocationReports($input: listLocationReportsInput) {
     listLocationReports(input: $input)
   }
 `;
 
 export const listPendingReviewQuestions = /* GraphQL */ `
-  query ListPendingReviewQuestions($next_token: [object Object], $organization_id: [object Object], $question_review_status: [object Object]) {
+  query ListPendingReviewQuestions($next_token: String, $organization_id: String, $question_review_status: questionReviewStatusEnum!) {
     listPendingReviewQuestions(next_token: $next_token, organization_id: $organization_id, question_review_status: $question_review_status)
   }
 `;
 
 export const listQuestionReports = /* GraphQL */ `
-  query ListQuestionReports($input: [object Object]) {
+  query ListQuestionReports($input: listQuestionReportsInput) {
     listQuestionReports(input: $input)
   }
 `;
 
 export const listQuestions = /* GraphQL */ `
-  query ListQuestions($input: [object Object]) {
+  query ListQuestions($input: listQuestionsInput2) {
     listQuestions(input: $input)
   }
 `;
 
 export const listQuizQuestions = /* GraphQL */ `
-  query ListQuizQuestions($quiz_id: [object Object]) {
+  query ListQuizQuestions($quiz_id: String) {
     listQuizQuestions(quiz_id: $quiz_id)
   }
 `;
 
 export const listQuizQuestions2 = /* GraphQL */ `
-  query ListQuizQuestions2($input: [object Object]) {
+  query ListQuizQuestions2($input: listQuizQuestions2Input) {
     listQuizQuestions2(input: $input)
   }
 `;
 
 export const listQuizRunLeaderboard = /* GraphQL */ `
-  query ListQuizRunLeaderboard($input: [object Object]) {
+  query ListQuizRunLeaderboard($input: listQuizRunLeaderboardInput) {
     listQuizRunLeaderboard(input: $input)
   }
 `;
 
 export const listQuizRunReports = /* GraphQL */ `
-  query ListQuizRunReports($input: [object Object]) {
+  query ListQuizRunReports($input: listQuizRunReportsInput) {
     listQuizRunReports(input: $input)
   }
 `;
 
 export const listQuizRunValidationErrors = /* GraphQL */ `
-  query ListQuizRunValidationErrors($input: [object Object]) {
+  query ListQuizRunValidationErrors($input: listQuizRunValidationErrorsInput) {
     listQuizRunValidationErrors(input: $input)
   }
 `;
 
 export const listQuizStats = /* GraphQL */ `
-  query ListQuizStats($input: [object Object]) {
+  query ListQuizStats($input: listQuizStatsInput) {
     listQuizStats(input: $input)
   }
 `;
 
 export const listQuizarreQuizRuns = /* GraphQL */ `
-  query ListQuizarreQuizRuns($input: [object Object]) {
+  query ListQuizarreQuizRuns($input: listQuizarreQuizRunsInput) {
     listQuizarreQuizRuns(input: $input)
   }
 `;
 
 export const listQuizzes = /* GraphQL */ `
-  query ListQuizzes($input: [object Object]) {
+  query ListQuizzes($input: listQuizzesInput) {
     listQuizzes(input: $input)
   }
 `;
 
 export const listSignUpUsers = /* GraphQL */ `
-  query ListSignUpUsers($input: [object Object]) {
+  query ListSignUpUsers($input: listSignUpUsersInput) {
     listSignUpUsers(input: $input)
   }
 `;
 
 export const listTeamLeaderboard = /* GraphQL */ `
-  query ListTeamLeaderboard($input: [object Object]) {
+  query ListTeamLeaderboard($input: listTeamLeaderboardInput) {
     listTeamLeaderboard(input: $input)
   }
 `;
 
 export const listTeamUsers = /* GraphQL */ `
-  query ListTeamUsers($input: [object Object]) {
+  query ListTeamUsers($input: listTeamUsersInput) {
     listTeamUsers(input: $input)
   }
 `;
 
 export const listTeams = /* GraphQL */ `
-  query ListTeams($input: [object Object]) {
+  query ListTeams($input: listTeamsInput) {
     listTeams(input: $input)
   }
 `;
 
 export const listUserActivities = /* GraphQL */ `
-  query ListUserActivities($input: [object Object]) {
+  query ListUserActivities($input: listUserActivitiesInput) {
     listUserActivities(input: $input)
   }
 `;
 
 export const listUserQuizAttemptQuestions = /* GraphQL */ `
-  query ListUserQuizAttemptQuestions($input: [object Object]) {
+  query ListUserQuizAttemptQuestions($input: listUserQuizAttemptQuestionsInput) {
     listUserQuizAttemptQuestions(input: $input)
   }
 `;
 
 export const listUserQuizAttempts = /* GraphQL */ `
-  query ListUserQuizAttempts($input: [object Object]) {
+  query ListUserQuizAttempts($input: listUserQuizAttemptsInput) {
     listUserQuizAttempts(input: $input)
   }
 `;
 
 export const listUsers = /* GraphQL */ `
-  query ListUsers($input: [object Object]) {
+  query ListUsers($input: listUserInput) {
     listUsers(input: $input)
   }
 `;
 
 export const searchUser = /* GraphQL */ `
-  query SearchUser($input: [object Object]) {
+  query SearchUser($input: searchUserInput) {
     searchUser(input: $input)
   }
 `;
