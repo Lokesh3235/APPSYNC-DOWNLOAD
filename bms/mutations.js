2076e60e8711f0b5869957c601a0820784aa87bb
Lokesh Kumar <7093lokesh@gmail.com>
2026-03-16 07:06:17 +0000

    chore: update AppSync schema + types + operations (Mon Mar 16 07:06:17 UTC 2026)

---
+++ bms/mutations.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+
+export const addCreditsToUser = /* GraphQL */ `
+  mutation AddCreditsToUser($input: addCreditsToUserInput) {
+    addCreditsToUser(input: $input)
+  }
+`;
+
+export const bookMarkQuestions = /* GraphQL */ `
+  mutation BookMarkQuestions($input: bookMarkQuestionsInput) {
+    bookMarkQuestions(input: $input)
+  }
+`;
+
+export const clearResponse = /* GraphQL */ `
+  mutation ClearResponse($input: clearResponseInput) {
+    clearResponse(input: $input)
+  }
+`;
+
+export const createQuizAttempts = /* GraphQL */ `
+  mutation CreateQuizAttempts($input: createQuizAttemptsInput) {
+    createQuizAttempts(input: $input)
+  }
+`;
+
+export const endQuizAttempt = /* GraphQL */ `
+  mutation EndQuizAttempt($input: endQuizAttemptInput) {
+    endQuizAttempt(input: $input)
+  }
+`;
+
+export const generateS3UploadURL = /* GraphQL */ `
+  mutation GenerateS3UploadURL($input: commons3Input) {
+    generateS3UploadURL(input: $input)
+  }
+`;
+
+export const recordUserActivity = /* GraphQL */ `
+  mutation RecordUserActivity($input: recordUserActivityInput) {
+    recordUserActivity(input: $input)
+  }
+`;
+
+export const submitQuizQuestionAnswer = /* GraphQL */ `
+  mutation SubmitQuizQuestionAnswer($input: submitQuizQuestionAnswerInput) {
+    submitQuizQuestionAnswer(input: $input)
+  }
+`;
+
+export const switchToATeam = /* GraphQL */ `
+  mutation SwitchToATeam($input: switchToATeamInput) {
+    switchToATeam(input: $input)
+  }
+`;
+
+export const updateProfile = /* GraphQL */ `
+  mutation UpdateProfile($input: updateProfileInput) {
+    updateProfile(input: $input)
+  }
+`;
---
+++ bms/queries.js
@@ -0,0 +1,87 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+
+export const GetParticularQuestionDetailsOfAttempt = /* GraphQL */ `
+  query GetParticularQuestionDetailsOfAttempt($input: GetParticularQuestionDetailsOfAttemptInput) {
+    GetParticularQuestionDetailsOfAttempt(input: $input)
+  }
+`;
+
+export const getAllTeamsOfUser = /* GraphQL */ `
+  query GetAllTeamsOfUser {
+    getAllTeamsOfUser
+  }
+`;
+
+export const getAttemptDetails = /* GraphQL */ `
+  query GetAttemptDetails($input: getAttemptDetailsInput) {
+    getAttemptDetails(input: $input)
+  }
+`;
+
+export const getCurrentUserDetails = /* GraphQL */ `
+  query GetCurrentUserDetails {
+    getCurrentUserDetails
+  }
+`;
+
+export const getDocumentPresignedURL = /* GraphQL */ `
+  query GetDocumentPresignedURL($input: commons3Input) {
+    getDocumentPresignedURL(input: $input)
+  }
+`;
+
+export const getQuestionDetails = /* GraphQL */ `
+  query GetQuestionDetails($input: getQuestionDetailsInput) {
+    getQuestionDetails(input: $input)
+  }
+`;
+
+export const listBmsSettings = /* GraphQL */ `
+  query ListBmsSettings {
+    listBmsSettings
+  }
+`;
+
+export const listBookMarksOfUser = /* GraphQL */ `
+  query ListBookMarksOfUser($input: listBookMarksOfUserInput) {
+    listBookMarksOfUser(input: $input)
+  }
+`;
+
+export const listQuizAttemptQuestions = /* GraphQL */ `
+  query ListQuizAttemptQuestions($input: listQuizAttemptQuestionsInput) {
+    listQuizAttemptQuestions(input: $input)
+  }
+`;
+
+export const listQuizAttempts = /* GraphQL */ `
+  query ListQuizAttempts {
+    listQuizAttempts
+  }
+`;
+
+export const listQuizQuestions = /* GraphQL */ `
+  query ListQuizQuestions($quiz_id: String) {
+    listQuizQuestions(quiz_id: $quiz_id)
+  }
+`;
+
+export const listQuizRunLeaderboard = /* GraphQL */ `
+  query ListQuizRunLeaderboard($input: listQuizRunLeaderboardInput) {
+    listQuizRunLeaderboard(input: $input)
+  }
+`;
+
+export const listQuizarreQuizRuns = /* GraphQL */ `
+  query ListQuizarreQuizRuns($input: listQuizarreQuizRunsInput) {
+    listQuizarreQuizRuns(input: $input)
+  }
+`;
+
+export const listTeamLeaderboard = /* GraphQL */ `
+  query ListTeamLeaderboard($input: listTeamLeaderboardInput) {
+    listTeamLeaderboard(input: $input)
+  }
+`;
